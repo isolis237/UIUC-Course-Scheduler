1d30b5b6487096b4bf7b185cda536e1cf4127861
Isolis237 <Isolis23761@gmail.com>
2020-10-13 21:38:26 -0500

    Fixed events not showing on calendar

--- classcalendar/src/component/EventCalendar2.js
+++ classcalendar/src/component/EventCalendar2.js
@@ -15,7 +15,37 @@ document.addEventListener('DOMContentLoaded', function() {
     slotMaxTime: "22:00:00",
     hiddenDays: [ 0, 6 ],
     allDaySlot: false,
-    headerToolbar: false,
+    headerToolbar: {
+      start: 'title',
+      center: '',
+      end: 'today prev,next'
+    },
+
+    eventSources: [
+      //events must have starts in sequence of yyyy-mm-ddT00:00:00 to show!
+      {
+        events: [
+          {
+            title  : 'event1',
+            start  : '2020-10-12T12:30:00'
+          },
+          {
+            title  : 'event2',
+            start  : '2020-10-15T12:30:00',
+            end    : '2020-10-15T14:30:00'
+          },
+          {
+            title  : 'event3',
+            start  : '2020-10-14T10:30:00',
+          }
+        ],
+        color: 'blue',
+        textColor: 'white'
+      }
+
+    ],
+
+
     dayHeaderFormat: { weekday: 'long' },
     expandRows: true
     });
